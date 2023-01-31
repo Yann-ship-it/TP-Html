1f4ffec57bf189fa021e7bae3e947d628b5f98cb
Yann-ship-it <yy.iris85@gmail.com>
2023-01-31 11:02:20 +0100

    Final header

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-/* Sélection des éléments HTML */
-let menu = document.getElementById('menu')
-let burger = document.getElementById('burger')
-let ul = document.querySelector('ul')
+const menu = document.getElementById('menu')
+const burger = document.getElementById('burger')
+const ul = document.querySelector('ul')
 
-/* gestionnaire d'événement sur le a#link pour venir changer l'attribution de la classe .open à la ul et au span#burger */
 menu.addEventListener('click', function(e) {
   e.preventDefault()
   burger.classList.toggle('open')
+  menu.classList.toggle('open')
   ul.classList.toggle('open')
 })
